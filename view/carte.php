c5835161ffa3a8549c173781b497459b56f656b9
Charles Poullain <charles@cubecom.fr>
2017-10-08 19:02:37 +0200

    new box carte

--- view/carte.php
+++ view/carte.php
@@ -2,13 +2,26 @@
 include 'commons/header.php';
 include 'commons/footer.php';
 use RedBeanPHP\R;
-/*
+
 R::setup( 'mysql:host=localhost;dbname=optimoov','root', null);
-$borne = R::findAll('bornes'); //reloads our book
-echo '<PRE>';
-var_dump($borne);
-echo '</PRE>';
-*/
+$borne = R::findAll('bornes');/*
+for ($i=1; $i < sizeof($borne); $i++) {
+  echo '<PRE>';
+  var_dump($borne[$i]["lat"],$borne[$i]["lng"]);
+  echo '</PRE>';
+
+}*/
+//id
+//id_station
+//adresse
+//Ville
+//code_postal
+//lat
+//lng
+//type_recharge
+//nbr_points_recharge
+//type_connecteur_id
+
 ?>
 <link rel="stylesheet" href="../web/css/bootstrap.css" ;?>
 <link rel="stylesheet" href="../web/css/custom.css">
@@ -44,10 +57,34 @@ echo '</PRE>';
     </div>
     <script>
 
-    var locations = [
-        ['Espace Villeneuve - Route de Challans', 46.7435, -1.61386, 4],
-        ['Rue Louis Pasteur', 46.597,-1.4523, 5]
-        ];
+
+var locations = [<?php for ($i=1; $i < sizeof($borne)+1; $i++) {
+  if($i < sizeof($borne))
+  {
+      echo "["."\"".
+      $borne[$i]["type_recharge"]."\"".","
+      .$borne[$i]["lat"].","
+      .$borne[$i]["lng"].","
+      ."\"".$borne[$i]["id_station"]."\"".","
+      ."\"".$borne[$i]["adresse"]."\"".","
+      ."\"".$borne[$i]["ville"]."\"".","
+      ."\"".$borne[$i]["type_connecteur_id"]."\"".","
+      ."\"".$borne[$i]["nbr_points_recharge"]."\"".
+      "],";
+    }else{
+      echo "["."\"".
+      $borne[$i]["type_recharge"]."\"".","
+      .$borne[$i]["lat"].","
+      .$borne[$i]["lng"].","
+      ."\"".$borne[$i]["id_station"]."\"".","
+      ."\"".$borne[$i]["adresse"]."\"".","
+      ."\"".$borne[$i]["ville"]."\"".","
+      ."\"".$borne[$i]["type_connecteur_id"]."\"".","
+      ."\"".$borne[$i]["nbr_points_recharge"]."\"".
+      "]";
+    }
+  } ?>
+];
 
     function initialize() {
 
@@ -71,22 +108,58 @@ echo '</PRE>';
         for (i = 0; i < locations.length; i++)
          {
 
-         var loan = locations[i][0]
+         var typeRecharge = locations[i][0]
          var lat = locations[i][1]
          var long = locations[i][2]
+         var id_station = locations[i][3];
+         var adresse = locations[i][4]
+         var ville = locations[i][5]
+         var type_connecteur_id = locations[i][6]
+         var nbr_points_recharge = locations[i][7]
+
+      //   var code_postal = locations[i][6]
+
 
 
          latlngset = new google.maps.LatLng(lat, long);
 
           var marker = new google.maps.Marker({
-                  map: map, title: loan , position: latlngset,
+                  map: map, title: typeRecharge , position: latlngset,
                   icon:"../web/assets/markers/type1.png"
 
                 });
-                map.setCenter(marker.getPosition())
+          map.setCenter(marker.getPosition())
+
+          var type_connecteur;
+
+          switch (type_connecteur_id) {
+            case "1":
+              type_connecteur = "EF-T3"
+              break;
+            case "2":
+              type_connecteur = "EF-T3/EF/T2"
+
+              break;
+            case "3":
+              type_connecteur = "T2-CHADEMO-COMBO"
+
+              break;
+          }
 
 
-                var content = "Loan Number: " + loan +  '</h3>'
+          var data = "<div class='infoBorne'>" + "<div class='titreBox'><center>"+id_station+"</center></div>"+"</br>"
+          +"</r>"+"<p>Type de borne : " + typeRecharge
+          +"</p></r>"+"<p>Adresse : " + adresse
+          + "</p></r>"+"<p>Ville : " + ville
+          +"</p></r>"+"<p>Types de connecteur : " + type_connecteur
+          +"</p></r>"+"<p>Nombre de points de recharge : " + nbr_points_recharge+
+          "</p></div>";
+
+          var content ="<div class='card card-outline-success text-xs-center'>"+
+          "<div class='card-block'><blockquote class='card-blockquote'>"+
+          data+
+          "</blockquote></div></div>";
+
 
           var infowindow = new google.maps.InfoWindow()
 
